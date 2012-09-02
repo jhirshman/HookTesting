52adeac32441ccf158556546993cc6757997f4e5
jhirshman4@gmail.com <jhirshman4@gmail.com>
2012-09-01 22:49:49 -0500

    Second Wave of Events

---
+++ createHookCURL.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+curl -v 'https://api.github.com/repos/jhirshman/HookTesting/hooks' -d '{
+	"name": "web",
+	"active": true,
+	"config": {
+			  "url": "http://hubhub2."
+	}
+	"events" : [
+	    "commit_comment",
+  		"create",
+    	"delete",
+    	"download",
+    	"follow",
+    	"fork",
+    	"fork_apply",
+    	"gist",
+    	"gollum",
+    	"issue_comment",
+    	"issues",
+    	"member",
+    	"public",
+    	"pull_request",
+    	"pull_request_review_comment",
+    	"push",
+    	"status",
+    	"team_add",
+    	"watch"
+	]
+}'
